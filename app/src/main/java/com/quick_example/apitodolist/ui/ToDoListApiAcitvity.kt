e670d4fc5d4f2f32bd990a0089391652cf8155ab
Jayybee21 <jadbeydoun26@gmail.com>
2022-06-25 18:04:51 +0300

    Modifications made in the activity class and xml files

--- app/src/main/java/com/quick_example/apitodolist/ui/ToDoListApiAcitvity.kt
+++ app/src/main/java/com/quick_example/apitodolist/ui/ToDoListApiAcitvity.kt
@@ -2,14 +2,67 @@ package com.quick_example.apitodolist.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.quick_example.apitodolist.databinding.ActivityToDoListApiAcitvityBinding
+import com.quick_example.apitodolist.service.RetrofitTodoApiData
+import com.quick_example.apitodolist.ui.utils.ToDoListAdapter
+import retrofit2.HttpException
+import java.io.IOException
 
 class ToDoListApiAcitvity : AppCompatActivity() {
+    private lateinit var todoadapter: ToDoListAdapter
+
     private lateinit var binding: ActivityToDoListApiAcitvityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        supportActionBar?.hide()
         super.onCreate(savedInstanceState)
         binding = ActivityToDoListApiAcitvityBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupRecView() //calling our private recyclerview
+
+        //coroutine scope used for loading the data online
+        lifecycleScope.launchWhenCreated {
+            //calling progress call to make it visible
+            binding.progressbarTodoApi.isVisible= true
+
+            //surrounding with try catch in case user has issues with connection or else
+
+            val response = try {
+                //todos list called
+                RetrofitTodoApiData.todoapi.getTodos()
+            }catch(e: IOException){
+                Log.d("ApiResultError", "Check internet connection ")
+                binding.progressbarTodoApi.isVisible= false
+                Toast.makeText(this@ToDoListApiAcitvity,"Connection Error !",Toast.LENGTH_SHORT).show()
+                return@launchWhenCreated
+            }catch(e: HttpException){
+                Log.d("ApiResultError", "Unexpected response")
+                binding.progressbarTodoApi.isVisible= false
+                Toast.makeText(this@ToDoListApiAcitvity,"Server Error !",Toast.LENGTH_SHORT).show()
+                return@launchWhenCreated
+            }
+           if(response.isSuccessful && response.body() != null){
+               //filling data following the json received from our api !
+                todoadapter.todos = response.body()!!
+           }else{
+               Toast.makeText(this@ToDoListApiAcitvity,"No data returned !",Toast.LENGTH_SHORT).show()
+               Log.d("ApiResultError", "No results were returned !")
+           }
+            binding.progressbarTodoApi.isVisible = false
+        }
+    }
+
+
+    //creating own function for recyclerview to not always call recyclerview. ....
+    private fun setupRecView() = binding.rvTodoApi.apply{
+        todoadapter = ToDoListAdapter()
+        adapter = todoadapter
+        layoutManager = LinearLayoutManager(this@ToDoListApiAcitvity)
     }
 }
--- app/src/main/java/com/quick_example/apitodolist/ui/utils/ToDoListAdapter.kt
+++ app/src/main/java/com/quick_example/apitodolist/ui/utils/ToDoListAdapter.kt
@@ -2,23 +2,45 @@ package com.quick_example.apitodolist.ui.utils
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncDifferConfig
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.quick_example.apitodolist.databinding.SingleItemToDoListApiBinding
 import com.quick_example.apitodolist.db.TodoSkeleton
+import com.quick_example.apitodolist.ui.ToDoListApiAcitvity
 
-class ToDoListAdapter(var items:List<TodoSkeleton>): RecyclerView.Adapter<ToDoListAdapter.TodoViewHolder>() {
+class ToDoListAdapter: RecyclerView.Adapter<ToDoListAdapter.TodoViewHolder>() {
 
     inner class TodoViewHolder(val binding:SingleItemToDoListApiBinding): RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
         return TodoViewHolder(SingleItemToDoListApiBinding.inflate(LayoutInflater.from(parent.context),parent,false))
     }
-    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
-        var currentToDoItem = items[position]
-        holder.binding.txtSingleItem.text = currentToDoItem.title
-        holder.binding.chkboxSingleItem.isChecked = currentToDoItem.completed
-     }
-    override fun getItemCount(): Int {
-        return items.size
+
+    //new function used to reduce api callback time if already exits
+    private val diffCallBack = object : DiffUtil.ItemCallback<TodoSkeleton>(){
+        override fun areItemsTheSame(oldItem: TodoSkeleton, newItem: TodoSkeleton): Boolean {
+            return oldItem.id == newItem.id
+        }
+        override fun areContentsTheSame(oldItem: TodoSkeleton, newItem: TodoSkeleton): Boolean {
+            return oldItem == newItem
+        }
     }
+    //next we call this function to check fr us the values
+    private val differ = AsyncListDiffer(this,diffCallBack)
+
+    //lastly we either get or set our values
+    var todos : List<TodoSkeleton>
+        get() = differ.currentList
+        set(value) {differ.submitList(value)}
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        holder.binding.apply {
+            txtSingleItem.text = todos[position].title
+            chkboxSingleItem.isChecked = todos[position].completed
+        }
+     }
+    override fun getItemCount() = todos.size
+
 }
